ac4ad7f81e2705ee150b3fead5b3b443328d0b89
Saisheshu <saisheshu@gmail.com>
2016-05-04 12:14:23 +0530

    Java_Assignment3_Session1
    2 Examples that a Integer Variable cannot store Other data types.

---
+++ src/Assignment_session1/Assignment3.java
@@ -0,0 +1,28 @@
+package Assignment_session1;
+
+public class Assignment3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Example - 1:
+		int x;
+		x=true; /* here "true" is a data type of boolean, so integer variable cannot store the data
+		
+		* type of boolean.
+		*/
+		
+		
+		// Example -2:
+		
+		int y;
+		
+		y="ACADGLID"; /* here "ACADGLID" is a data type of String, so integer variable cannot store the data
+		
+		* type of String.
+		*/
+
+	}
+
+}
